30d7e232594a9941560d7e1de0342b621421ad0c
Rajasekhar <bammidirajasekhar.2018.eee@rajalakshmi.edu.in>
2022-05-15 22:20:37 +0530

    final update code

--- 3_Implementation/Src/main.c
+++ 3_Implementation/Src/main.c
@@ -27,12 +27,12 @@ void setOutput(void);
 void delay(int par);
 void off();
 int veh_Off();
-
+void hold_On_Off();
 
 int main(void)
 {
 	uint16_t count = 1;
-	/*Setting GREEN LED PIN*/
+	/*Setting GREEN LED PIN With Clock*/
 	GPIO_Handle_t INP,LED;
 	LED.pGPIOx = GPIOD;
 	LED.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_12;
@@ -42,7 +42,7 @@ int main(void)
 	GPIO_PeriClockControl(GPIOD, ENABLE);
 	GPIO_Init(&LED);
 
-	/*Setting ORANGE LED PIN*/
+	/*Setting ORANGE LED PIN With Clock*/
 	LED.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_13;
 	LED.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
 	LED.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
@@ -50,7 +50,7 @@ int main(void)
 	GPIO_PeriClockControl(GPIOD, ENABLE);
 	GPIO_Init(&LED);
 
-	/*Setting RED LED pin*/
+	/*Setting RED LED pin With Clock*/
 	LED.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_14;
 	LED.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
 	LED.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
@@ -58,7 +58,7 @@ int main(void)
 	GPIO_PeriClockControl(GPIOD, ENABLE);
 	GPIO_Init(&LED);
 
-	/*Setting RED LED pin*/
+	/*Setting RED LED pin With Clock*/
 	LED.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_15;
 	LED.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
 	LED.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
@@ -73,19 +73,16 @@ int main(void)
 
 	while(1)
 	{
-		//GPIO_WriteToOutputPin(GPIOA, GPIO_PIN_NO_0, DISABLE);//setting PAO '0' initially
-		if(GPIO_ReadFromInputPin(GPIOA, GPIO_PIN_NO_0) == ENABLE){//checking for input as HIGH
-			//delay(10000000);
-			if(count == 1){
-				delay(20000000);
-				delay(20000000);
-				delay(20000000);
-				delay(20000000);
+
+		if(GPIO_ReadFromInputPin(GPIOA, GPIO_PIN_NO_0) == ENABLE){//checking for input as HIGH from Port PA0 pin.
+
+			if(count == 1){															//To Turn on RED LED with some 2 sec
+				hold_On_Off();
 				GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_14);//RED led
 				count++;
 
 			}
-			else if(count == 2){
+			else if(count == 2){													//Button pressed 2nd time
 				if(GPIO_ReadFromInputPin(GPIOA, GPIO_PIN_NO_0) == ENABLE)
 				while(count ==2){
 					if(slow() == 1){
@@ -95,7 +92,7 @@ int main(void)
 
 				}
 			}
-			else if(count == 3){
+			else if(count == 3){													//Button pressed 3rd time
 				while(count == 3){
 					if(medium() == 1){
 						count++;
@@ -103,7 +100,7 @@ int main(void)
 					}
 				}
 			}
-			else if(count == 4){
+			else if(count == 4){													//Button pressed 4th time
 				while(count == 4){
 					if(fast() == 1){
 						count++;
@@ -111,15 +108,15 @@ int main(void)
 					}
 				}
 			}
-			else if(count == 5){
+			else if(count == 5){													//Button pressed 5th time
 				off();
 				count++;
 			}
-			else if(count ==6){
+			else if(count ==6){														//Button pressed 6th time
 
 				if(veh_Off() == 1){
 					delay(HZ_4);
-					count =1;
+					count =1;														//To Restart the car in a press of 2 sec.
 				}
 
 			}
@@ -128,13 +125,14 @@ int main(void)
 
 }
 
-void delay(int par)
+void delay(int par)																	//This function is used to provide delay
 {
 	for (uint32_t i = 0; i < par ; i++);
 }
 
-int slow()
+int slow()																			//To run at 2nd Key press of the button
 {
+	/*Enabling all the BLUE GREEN ORANGE led on 2nd key press with some delay*/
 	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_15);
 	delay(HZ_1);
 	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_15, DISABLE);
@@ -147,15 +145,14 @@ int slow()
 	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_12);
 	delay(HZ_1);
 	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_12, DISABLE);
-	/*GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_15);
-	delay(50000000);
-	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_15, DISABLE);*/
+	/*If push button is pressed return 1*/
 	if(GPIO_ReadFromInputPin(GPIOA, GPIO_PIN_NO_0)){
 		return  1;
 	}
 	else
 	return 0;
 }
+/*Enabling all the BLUE GREEN ORANGE led on 3rd key press with some delay*/
 int medium(){
 	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_15);
 	delay(HZ_4);
@@ -169,16 +166,13 @@ int medium(){
 	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_12);
 	delay(HZ_4);
 	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_12, DISABLE);
-	/*GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_15);
-	delay(7500000);
-	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_15, DISABLE);*/
 	if(GPIO_ReadFromInputPin(GPIOA, GPIO_PIN_NO_0)){
 		return  1;
 	}
 	else
 		return 0;
 }
-
+/*Enabling all the BLUE GREEN ORANGE led on 4th key press with some delay*/
 int fast(){
 	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_15);
 	delay(HZ_8);
@@ -192,15 +186,14 @@ int fast(){
 	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_12);
 	delay(HZ_8);
 	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_12, DISABLE);
-	/*GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_15);
-	delay(100000);
-	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_15, DISABLE);*/
+
 	if(GPIO_ReadFromInputPin(GPIOA, GPIO_PIN_NO_0)){
 		return  1;
 	}
 	else
 		return 0;
 }
+/*Enabling all the BLUE GREEN ORANGE led on 5th key press with some delay*/
 
 void off(){
 
@@ -214,6 +207,7 @@ void off(){
 	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_15, DISABLE);
 
 }
+/*Enabling all the BLUE GREEN ORANGE led on 6th key press with some delay*/
 int veh_Off(){
 	GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_14, DISABLE);
 	if(GPIO_ReadFromInputPin(GPIOA, GPIO_PIN_NO_0)){
@@ -221,4 +215,11 @@ int veh_Off(){
 		}
 		else
 			return 0;
+}
+//For holding 2 sec press car turn on
+void hold_On_Off(){
+	delay(20000000);
+	delay(20000000);
+	delay(20000000);
+	delay(20000000);
 }
